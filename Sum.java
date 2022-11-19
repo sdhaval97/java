7c3f092929cc9e6f0edc655fd62f5ca2e20e88a1
sdhaval97 <sdhaval97@gmail.com>
2022-11-18 20:01:45 -0500

    printing sum of 2 numbers

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args) {
+
+        int a = 55;
+        int b = 65;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+        
+    }
+}
